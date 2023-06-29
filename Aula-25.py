da44aec2b2549127841d6560ddd03e8958483cba
Paulo Roger <133438456+Eng-Paulo@users.noreply.github.com>
2023-06-29 20:17:50 -0300

    Add files via upload

---
+++ Aula-25.py
@@ -0,0 +1,17 @@
+''' (Caracteres) (<>^) (quantidade)
+
+> - Esquerda
+< - Direita
+^ - Centro
+Conversion flags - !r !s !a
+'''
+
+variavel = "ABC"
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{1000.4873648123746}')
+print(f'{1000.4873648123746:.1f}')
+print('O hexadecimal de 1500 é {1500:08X}')
+print(f'{variavel!r}')
